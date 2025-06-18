063faf1e30f92c320ccad69306bfb7237fc49686
Nihar2123 <maddilinihar@gmail.com>
2025-06-18 14:32:42 +0530

    Message from dev

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ import "./projectWindow.css"
 import "./todoDisplay.css"
 import "./notes.css"
 import "./todoDialog.css"
+import odinImage from "./icons/The Odin Project/The Odin Project_idkh09PaW6_1.svg";
 
 //import {createTodoItem} from "./todos"
 import {createProject} from "./projects";
@@ -12,7 +13,9 @@ import projectList from "./projectmanager"
 projectList.addProject();
 projectList.addProject("Home", 'Thing to do when free');
 projectList.list[0].addTodo("Test", "Trying to create a Todo Item", new Date(), '9');
+projectList.list[0].addTodo("Try Medium", "", null, '5');
 projectList.list[0].todos[0].addCheckList('SubTask no. 1');
+projectList.openProjectWindow(projectList.list[0]);
 // projectList.list[0].displayProject();
 // projectList.list[1].displayProject();
 
@@ -22,4 +25,13 @@ projectList.list[0].todos[0].addCheckList('SubTask no. 1');
 
 // const x = createTodoItem("Test", "Trying to create a Todo Item", new Date(), '9');
 // x.addCheckListItem('SubTask no. 1');
-console.log(projectList);
+//console.log(projectList);
+
+const infoDiv = document.querySelector(".info");
+const infoDivText = document.createElement("div");
+const image = document.createElement("img");
+image.src = odinImage;
+infoDivText.innerText = 'Created by Nihar M as part of the Odin Project'
+infoDiv.appendChild(image);
+infoDiv.appendChild(infoDivText);
+//console.log(infoDiv);
